1beddfe8fcb4e1f5fd55c22a8545955688cb10cf
mohdahtesham09 <ahteshammohd094@gmail.com>
2026-07-02 21:21:25 +0530

    Fix Syntax typo

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ app.get("/health", (req, res)=>{
     return res.status(200).json({message: "Server is healthy"});
 })
 
-app.get("/", (req, res0=>{
+app.get("/", (req, res) =>{
     return res.status(200).json({message: "Welcome to the AWS server"})
-}))
+})
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
