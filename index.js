b77568fa664a41a669587be222cd9f3da3002627
mohdahtesham09 <ahteshammohd094@gmail.com>
2026-07-03 15:39:44 +0530

    after setup ci/cd

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ express.json();
 const PORT = process.env.PORT || 5000;
 
 app.get("/health", (req, res)=>{
-    return res.status(200).json({message: "Server is healthy"});
+    return res.status(200).json({message: "Server is healthy and running 🟢"});
 })
 
 app.get("/", (req, res) =>{
-    return res.status(200).json({message: "Welcome to the AWS server v2"})
+    return res.status(200).json({message: "Welcome to the AWS server v3 👍"})
 })
 
 app.listen(PORT, ()=>{
